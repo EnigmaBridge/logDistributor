7933990eb7eaefa80330048de3953eb14f87676e
Dusan Klinec <dusan.klinec@gmail.com>
2016-08-03 22:42:28 +0200

    hibernate support added, dialect added

---
+++ webServer/src/main/java/com/enigmabridge/log/distributor/model/Client.java
@@ -0,0 +1,49 @@
+package com.enigmabridge.log.distributor.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Created by dusanklinec on 03.08.16.
+ */
+@Entity
+@Table()
+public class Client {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    private String clientId;
+
+    @Column
+    @OneToMany
+    @JoinColumn(name = "uoid")
+    private List<UserObject> objects;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public List<UserObject> getObjects() {
+        return objects;
+    }
+
+    public void setObjects(List<UserObject> objects) {
+        this.objects = objects;
+    }
+}
---
+++ webServer/src/main/java/com/enigmabridge/log/distributor/model/UserObject.java
@@ -0,0 +1,58 @@
+package com.enigmabridge.log.distributor.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by dusanklinec on 03.08.16.
+ */
+@Entity
+@Table()
+public class UserObject {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    @Column
+    private String apiKey;
+
+    @Column
+    private Integer uoType;
+
+    @Column
+    private Integer uoId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public Integer getUoType() {
+        return uoType;
+    }
+
+    public void setUoType(Integer uoType) {
+        this.uoType = uoType;
+    }
+
+    public Integer getUoId() {
+        return uoId;
+    }
+
+    public void setUoId(Integer uoId) {
+        this.uoId = uoId;
+    }
+}
