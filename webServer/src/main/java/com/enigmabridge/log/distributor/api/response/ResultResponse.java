25c390f9c1a6a8304d17dfc85ca872953f0e7780
Dusan Klinec <dusan.klinec@gmail.com>
2016-08-04 16:46:36 +0200

    success response needs getters/setters

--- webServer/src/main/java/com/enigmabridge/log/distributor/api/response/ResultResponse.java
+++ webServer/src/main/java/com/enigmabridge/log/distributor/api/response/ResultResponse.java
@@ -13,4 +13,20 @@ public class ResultResponse extends GeneralResponse {
      * Might be JSONObject or another serializable object
      */
     protected Object result;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
 }
