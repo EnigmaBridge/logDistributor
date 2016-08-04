a7f849b052596d2facf6ea7dd1e66dc4681d8dd1
Dusan Klinec <dusan.klinec@gmail.com>
2016-08-04 18:03:48 +0200

    user object equals - on apices, uotype, uoid
    - detect duplicates

--- webServer/src/main/java/com/enigmabridge/log/distributor/db/model/UserObject.java
+++ webServer/src/main/java/com/enigmabridge/log/distributor/db/model/UserObject.java
@@ -55,4 +55,25 @@ public class UserObject {
     public void setUoId(Integer uoId) {
         this.uoId = uoId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserObject that = (UserObject) o;
+
+        if (apiKey != null ? !apiKey.equals(that.apiKey) : that.apiKey != null) return false;
+        if (uoType != null ? !uoType.equals(that.uoType) : that.uoType != null) return false;
+        return uoId != null ? uoId.equals(that.uoId) : that.uoId == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = apiKey != null ? apiKey.hashCode() : 0;
+        result = 31 * result + (uoType != null ? uoType.hashCode() : 0);
+        result = 31 * result + (uoId != null ? uoId.hashCode() : 0);
+        return result;
+    }
 }
