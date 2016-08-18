b3290c296b9a6dcd2f7cae7ce8435ef383a30c25
Dusan Klinec <dusan.klinec@gmail.com>
2016-08-18 17:13:13 +0200

    install script refactoring

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,16 +5,16 @@ PRODUCT=logdist
 
 # Installation
 sudo git clone https://github.com/EnigmaBridge/logDistributor.git "/opt/${PRODUCT}" && \
-sudo mkdir -p "/etc/${PRODUCT}" && \
-sudo cp "/opt/${PRODUCT}/webServer/config/appConfig.yml.example" "/etc/${PRODUCT}" && \
-sudo cp "/opt/${PRODUCT}/webServer/config/logback.xml.example" "/etc/${PRODUCT}" && \
-sudo mkdir -p "/var/log/${PRODUCT}" && \
-ln -s "/opt/${PRODUCT}/scripts/${PRODUCT}" "/etc/init.d/${PRODUCT}" && \
-cd "/etc/${PRODUCT}" && \
-sudo bash "/opt/${PRODUCT}/createKeystore.sh" && \
-cd "/opt/${PRODUCT}" && \
-bash buildBoot.sh && \
-echo "OK"
+    sudo mkdir -p "/var/log/${PRODUCT}" && \
+    sudo mkdir -p "/etc/${PRODUCT}" && \
+    sudo cp "/opt/${PRODUCT}/webServer/config/appConfig.yml.example" "/etc/${PRODUCT}" && \
+    sudo cp "/opt/${PRODUCT}/webServer/config/logback.xml.example" "/etc/${PRODUCT}" && \
+    sudo ln -s "/opt/${PRODUCT}/scripts/${PRODUCT}" "/etc/init.d/${PRODUCT}" && \
+    cd "/etc/${PRODUCT}" && \
+    sudo bash "/opt/${PRODUCT}/createKeystore.sh" && \
+    cd "/opt/${PRODUCT}" && \
+    bash buildBoot.sh && \
+    echo "OK"
 
 
 
