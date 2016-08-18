f5659afe3a44d51cee3fed791a209e6da4ae5a7c
Dusan Klinec <dusan.klinec@gmail.com>
2016-08-18 09:38:06 +0200

    scripts added

---
+++ scripts/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+APP_CONFIG="/etc/logdist/appConfig.yml"
+LOG_FILE="/var/log/logdist/logdist.log"
+LOG_CONFIG="/etc/logdist/logback.xml"
+JAR_PATH="/opt/logdist/webServer/build/libs/webServer-1.0-SNAPSHOT.jar"
+
+# ***********************************************
+# ***********************************************
+
+ARGS=" -Dconfig.location=${APP_CONFIG} -Dlogging.logfile=${LOG_FILE} "
+#-Dlogback.configurationFile=${LOG_CONFIG}"
+
+exec java $ARGS -jar "${JAR_PATH}" $*
+
